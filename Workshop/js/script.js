af88050541b07ccbb819c58b0d0563c4fb01137f
frepetti <franco.l.repetti@gmail.com>
2018-05-17 20:20:53 -0300

    Refinado de Codigo

--- Workshop/js/script.js
+++ Workshop/js/script.js
@@ -2,7 +2,7 @@
 
 //Para guardar la noticia en el LocalStorage, necesito definir un objeto literal o una funcion de construccion para convertirla en objeto
 
-var noticias = [];
+var noticias = []; //Variable Global. Va a ser utilizada por varias funciones
 
 //Con esta funcion me traigo las noticias que estan guardadas en el LocalStorage
 function getNoticias() {
@@ -17,8 +17,10 @@ function getNoticias() {
 
 }
 
-function Noticia(titulo, descripcion, imagen, categoria){
-    
+//Funcion de construccion
+function Noticia(id, titulo, descripcion, imagen, categoria) {
+
+    this.id = id;
     this.titulo = titulo;
     this.descripcion = descripcion;
     this.imagen = imagen;
@@ -40,42 +42,153 @@ function dibujarNoticias() {
         li.find("img").attr("src", noticias[i].imagen);
         li.appendTo("#noticias");
 
-        
+
     }
-    
+
 }
 
+//Opcion 1
+/*
+var id = 0; //Variable global, cualquier funcion puede acceder a ella
+*/
+
+function generarIdLocalStorage() {
+    var id = 0;
+    var dato = localStorage.getItem("id");//Busco el objeto "id" en el LocalStorage, si no lo encuentra, id=0
+
+    if (dato !== null) {
+        id = parseInt(dato) + 1;
+    }
+    localStorage.setItem("id", id);//Subo el objeto "id" al LocalStorage con su valor
+    return id;
+}
+
+//Opcion 2
+
+function generarIdLastChild() {
+
+    var id = 0;
+
+    if ($("li:last-child").attr("id") !== undefined) {//Porque el last-child no esta definido en la primera iteracion.
+        id = parseInt($("li:last-child").attr("id")) + 1;
+    }
+
+    return id;
+}
+
+//Ordenar Noticias
+//Lo mas simple es borrar las noticias ya dibujadas en el sitio y volver a escribirlas ordenadas.
+
+
+//Funciones con el criterio para ordenar
+function ordenarPorTituloAsc(noticiaA, noticiaB) {
+    var resultado = 0; //Son iguales
+    if (noticiaA.titulo > noticiaB.titulo) {//noticiaA mayor a noticiaB
+        resultado = 1;
+    }
+
+    if (noticiaA.titulo < noticiaB.titulo) { //noticiaA menor a noticiaB
+        resultado = -1;
+    }
+    return resultado;
+}
+
+function ordenarPorTituloDes(noticiaA, noticiaB) {
+    var resultado = 0; //Son iguales
+    if (noticiaA.titulo > noticiaB.titulo) {
+        resultado = -1;
+    }
+
+    if (noticiaA.titulo < noticiaB.titulo) {
+        resultado = 1;
+    }
+    return resultado;
+}
+
+function ordenarPorID(noticiaA, noticiaB) {
+    var resultado = 0; //Son iguales
+    if (noticiaA.id > noticiaB.id) {
+        resultado = 1;
+    }
+
+    if (noticiaA.id < noticiaB.id) {
+        resultado = -1;
+    }
+    return resultado;
+}
+
+
+
+
+
+//Esta funcion va a crear las noticias nuevas
 function vincularElementos() {
 
     $("#boton").click(function () {
+        //Gnerar un ID
+        //var id = generarIdLocalStorage(); //Opcion 1
+        var id = generarIdLastChild(); //Opcion 2
 
-        var noticia = new Noticia($("#titulo").val(),  
-                                  $("#descripcion").val(),
-                                  $("#imagen").val(), 
-                                  $("#categoria").val());
+        //Validar que el formulario este completo
+        if ($("#titulo").val() === "" || $("#descripcion").val() === "" || $("#imagen").val() === "") {
+            alert("Por favor complete todos los campos");
+        } else {
+            //Crear el objeto noticia tomando los valores ingresados en el formulario
+            var noticia = new Noticia(id,
+                $("#titulo").val(),
+                $("#descripcion").val(),
+                $("#imagen").val(),
+                $("#categoria").val());
 
-        noticias.push(noticia);
+            noticias.push(noticia);
 
-        localStorage.setItem("noticias", JSON.stringify(noticias));
+            localStorage.setItem("noticias", JSON.stringify(noticias));
 
-        var noticiaTemplate = $("#noticia-template").html();
-        var li = $(noticiaTemplate);// Esto nos devuelve un objeto de jQuery que contiene el HTML
+            var noticiaTemplate = $("#noticia-template").html(); //Me lo guarda como string
+            var li = $(noticiaTemplate);// Esto nos devuelve un objeto de jQuery que contiene el HTML
 
-        li.find("h3").text(noticia.titulo);
-        li.find("h4").text(noticia.categoria);
-        li.find("p").text(noticia.descripcion);
-        li.find("img").attr("src", noticia.imagen);
-        li.appendTo("#noticias");
+            li.attr("id", id);
+            li.find("h3").text(noticia.titulo);//Encuentra el elemento h3 y asigna el atributo titulo del objeto noticia
+            li.find("h4").text(noticia.categoria);
+            li.find("p").text(noticia.descripcion);
+            li.find("img").attr("src", noticia.imagen);
+            li.appendTo("#noticias");
 
-        /*
+            /*
 
-        //Si no usara el template, tendria que crear el li de la siguiente manera, pasandole los atributos del objeto noticias.
-        $("<li/>").text(noticias[i].titulo +
-                        noticias[i].descripcion +
-                        noticias[i].imagen +
-                        noticias[i].categoria).appendTo("#noticias");
-        */
+            //Si no usara el template, tendria que crear el li de la siguiente manera, pasandole los atributos del objeto noticias.
+            $("<li/>").text(noticias[i].titulo +
+                            noticias[i].descripcion +
+                            noticias[i].imagen +
+                            noticias[i].categoria).appendTo("#noticias");
+            */
+
+        }
+
     })
+
+    $("#ordenarPorAZ").click(function () {
+
+        noticias.sort(ordenarPorTituloAsc);//Le paso la funcion de ordenar como callback del sort
+        $("#noticias").empty();//Borro el contenedor "noticias"
+        dibujarNoticias();//Vuelvo a dibujar todas las noticias ya ordenadas
+
+    });
+
+    $("#ordenarPorZA").click(function () {
+
+        noticias.sort(ordenarPorTituloDes);
+        $("#noticias").empty();
+        dibujarNoticias();
+    });
+
+    $("#ordenarPorId").click(function () {
+
+        noticias.sort(ordenarPorID);
+        $("#noticias").empty();
+        dibujarNoticias(); 
+
+    });
 
 }
 
@@ -85,22 +198,23 @@ function iniciarPagina() {
     getNoticias();
     dibujarNoticias();
     vincularElementos();
+
 }
 
 
 $(document).ready(iniciarPagina);
 
 /*
-
     Guardar las noticias
     LocalStorage
     JSON
-
 */
 
-var noticiaTemplate = $("#noticia-template").html(); //Me va a traer el html del tag con la clase "noticia-template" y se lo va a asignar a la variable noticiaTemplate en forma de string
-
-var li = $(noticiaTemplate); //jQuery va a tomar el string y lo va a convertir en un objeto de HTML y se lo asigna a la variable
+/*
+$("#boton-borrar").click(function () {
+    
+})
+*/
 
 
 
